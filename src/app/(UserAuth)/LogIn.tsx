a2aa593330ade051b640225a1aba4b1e2d60dead
Eduardo-Pyment <edupymota@protonmail.com>
2024-04-19 13:02:38 -0300

    fix: Responsividade LogIn Responsividade da página LogIn aplicada

--- src/app/(UserAuth)/LogIn.tsx
+++ src/app/(UserAuth)/LogIn.tsx
@@ -13,125 +13,136 @@ import { router } from "expo-router";
 export default function LogIn() {
   return (
     <View style={styles.container}>
-      <LinearGradient
-        colors={["#ffffff", "#BBE7FF", "#2596BE"]}
-        style={{ flex: 1 }}
-      >
-        <View style={styles.containerImagemLogin}>
-          <Image
-            source={require("../../assets/UserAuth/teste.jpg")}
-            style={{ width: "100%", height: "100%" }}
-            blurRadius={0.5}
-            resizeMode="cover"
-          />
+            <LinearGradient colors={["#ffffff","#BBE7FF", "#2596BE"]} style={{flex: 1}}>
+            <View style={styles.containerImagemLogin}>
+                <Image
+                    source={ require("../../assets/UserAuth/teste.jpg" )}
+                    style={styles.containerImagemLoginImagem}
+                    resizeMode="cover" 
+                />
+            </View>
+            <View style={styles.containerForm}>
+                <TextInput
+                    style={styles.containerInput}
+                    placeholder="Digite seu e-mail"
+                    autoComplete="email"
+                    autoCapitalize="none"
+                    autoCorrect= {false}
+                    keyboardType="email-address"
+                    enterKeyHint="next"
+                    importantForAutofill="yes"
+                />
+                <TextInput
+                    style={styles.containerInput}
+                    placeholder="Digite sua senha"
+                    autoComplete="password"
+                    autoCapitalize="none"
+                    autoCorrect= {false}
+                    enterKeyHint="done"
+                    importantForAutofill="yes"
+                    secureTextEntry= {true}
+                />   
+                <TouchableOpacity 
+                style={styles.containerBotao}
+                >
+                <Image 
+                source={require("../../assets/UserAuth/logo_entrar.png")}
+                style={styles.containerImagemBotaoEntrar}
+                >
+                </Image>
+                <Text 
+                style={styles.botaoTexto}
+                onPress={ () => router.navigate("Home")}
+                >Entrar</Text>
+                </TouchableOpacity>
+                <View style={styles.Registro}>
+                    <Text style={styles.textoRegistro}>
+                        Ainda não possui cadastro?
+                    </Text> 
+                    <TouchableOpacity onPress={ () => router.navigate("SignIn")}>
+                        <Text style={{fontSize: 15, marginLeft: 5, fontStyle: "italic", textDecorationLine: "underline"}}>
+                            cadastre-se aqui
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+            </LinearGradient>
         </View>
-        <View style={styles.containerForm}>
-          {/*(<Text>E-mail</Text>*/}
-          <TextInput
-            style={styles.containerInput}
-            placeholder="Digite seu e-mail"
-          />
-          {/*<Text>Senha</Text>*/}
-          <TextInput
-            style={styles.containerInput}
-            placeholder="Digite sua senha"
-          />
-          <TouchableOpacity
-            style={styles.containerBotao}
-            onPress={() =>
-              router.navigate({
-                pathname: "../(Main)/Home",
-              })
-            }
-          >
-            <Image
-              source={require("../../assets/UserAuth/logo_entrar.png")}
-              style={styles.containerImagemBotaoEntrar}
-            ></Image>
-            <Text style={styles.botaoTexto}>Entrar</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() =>
-              router.navigate({
-                pathname: "./SignIn",
-              })
-            }
-          >
-            <Text style={styles.botaoRegistro}>
-              Ainda não possui cadastro? cadastre-se aqui
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </LinearGradient>
-    </View>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    width: "100%",
-    height: "100%",
-  },
-  containerImagemLogin: {
-    flex: 2,
-    borderRadius: 30,
-    maxWidth: 450,
-    opacity: 0.45,
-    width: "100%",
-    height: "100%",
-  },
-  containerForm: {
-    justifyContent: "space-around",
-    paddingHorizontal: 40,
-    position: "absolute",
-    width: "100%",
-    alignSelf: "center",
-    top: "50%",
-    gap: 25,
-    maxWidth: 400,
-    minWidth: 300,
-  },
-  containerBotao: {
-    alignSelf: "center",
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#ffffff",
-    borderRadius: 15,
-    width: 100,
-    height: 40,
-    flexDirection: "row",
-  },
-  containerInput: {
-    borderWidth: 1,
-    backgroundColor: "#ffffff",
-    borderColor: "#000000",
-    shadowColor: "#000000",
-    elevation: 15,
-    shadowOffset: { width: -1, height: 1 },
-    shadowOpacity: 0.9,
-    shadowRadius: 1,
-    paddingHorizontal: 10,
-    fontSize: 15,
-    fontWeight: "600",
-    borderRadius: 3,
-    paddingTop: 5,
-    paddingBottom: 5,
-  },
-  botaoTexto: {
-    fontWeight: "bold",
-    fontSize: 18,
-    textAlign: "center",
-  },
-  botaoRegistro: {
-    textDecorationStyle: "dashed",
-    fontStyle: "italic",
-    color: "#000000",
-    alignSelf: "center",
-  },
-  containerImagemBotaoEntrar: {
-    width: 20,
-    height: 20,
-    marginRight: 8,
-  },
-});
+    container:{
+        flex:1,
+        width: "100%",
+        height:"100%",
+    },
+    containerImagemLogin:{
+        flex:1,
+        maxWidth: 450,
+        opacity: 0.45,
+        width: "100%",
+        height: "100%",
+        alignSelf: "center",
+    },
+    containerImagemLoginImagem:{
+        width: "100%",
+        height: "100%",
+        alignSelf: "center",
+    },
+    containerForm:{
+        justifyContent:"space-around",
+        paddingHorizontal:40,
+        position: 'absolute',
+        width:"100%",
+        alignSelf: "center",
+        top: "50%",
+        gap: 25,
+        maxWidth: 400,
+        minWidth: 300,
+    },
+    containerBotao:{
+        alignSelf: "center",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#ffffff",
+        borderRadius: 15,
+        width: 100,
+        height:  40,
+        flexDirection: "row",
+    },
+    containerInput:{
+        borderWidth: 1,
+        backgroundColor: "#ffffff",
+        borderColor: "#000000",
+        shadowColor: "#000000",
+        elevation: 15,
+        shadowOffset:{width: -1,height: 1},
+        shadowOpacity: 0.9,
+        shadowRadius: 1,
+        paddingHorizontal: 10,
+        fontSize: 15,
+        fontWeight: "600",
+        borderRadius: 3,
+        paddingTop: 5,
+        paddingBottom: 5,
+    },
+    botaoTexto:{
+        fontWeight: "bold",
+        fontSize: 18,
+        textAlign: "center"
+    },
+    Registro:{
+        alignSelf: "center",
+        flexDirection: "row"
+    },
+    textoRegistro:{
+        fontSize: 15,
+        fontStyle: "italic",
+    },
+    containerImagemBotaoEntrar:{
+        width: 20,
+        height: 20,
+        marginRight: 8,
+    }
+})  
