63cab8ebc592b3bc72bacfa9c022d14c6ea34d00
Guilherme G <guigabriel2811914@gmail.com>
2024-04-20 00:52:40 -0300

    pre-finalização da area adicionar medicamentos

--- src/app/(Main)/(TabMenu)/AdMedicamentos.tsx
+++ src/app/(Main)/(TabMenu)/AdMedicamentos.tsx
@@ -22,20 +22,20 @@ export default function AdMedicamentos(){
             </View>
             <View style={styles.containerForm}>
                 <Text style={styles.containerTexto}
-                    >NOME DO PACIENTE:</Text>
-                    <TextInput
-                    style={styles.containerInput}
-                />
-                <Text style={styles.containerTexto}
-                    >MEDICAMENTO:</Text>
+                    >NOME DO PACIENTE:
+                </Text>
                 <TextInput
                     style={styles.containerInput}
                 />
+                    <Text style={styles.containerTexto}
+                    >MEDICAMENTO:</Text>
+                <TextInput
+                    style={styles.containerInput}
+                    />
+                <Text style={styles.containerTextoDosagem}>DOSAGEM:</Text>
+                <Text style={styles.containerTextoUso}>USO CONTÍNUO</Text>
             </View>
-            <View style={styles.containerTexto2}>
-                <Text style={styles.containerTexto}>DOSAGEM:</Text>
-                <Text style={styles.containerTexto}>USO CONTÍNUO</Text>
-            </View>
+            
             <View style={styles.containerMenor}>
                 <TextInput style={styles.containerInputDosagem}>
                 </TextInput>
@@ -53,32 +53,187 @@ export default function AdMedicamentos(){
                     <TextInput style={styles.containerInputPeriodo}>
                     </TextInput>
                 </View>
+                <View style={styles.containerCompriPeriodo}>
+                    <Text style={styles.containerTextoCompriPeriodo}>COMPRIMIDOS POR PERÍODO:</Text>
+                    <TextInput style={styles.containerInputCompriPeriodo}>
+                    </TextInput>
+                </View>
+                <View style={styles.containerMedico}>
+                    <Text style={styles.containerTextoMedico}>MÉDICO:</Text>
+                    <TextInput style={styles.containerInput}>
+                    </TextInput>
+                </View>
+                <View style={styles.containerVencimento}>
+                    <Text style={styles.containerTextoVencimento}>VENCIMENTO:</Text>
+                    <TextInput style={styles.containerInput}>
+                    </TextInput>
+                </View>
+                <View style={styles.containerBotaoSalvar}>
+                <TouchableOpacity 
+                style={styles.containerBotao}
+                >
+                
+                <Text style={styles.botaoTexto}>SALVAR</Text>
+                </TouchableOpacity>
+                </View>
+                <View style={styles.containerBotaoVoltar}>
+                <TouchableOpacity 
+                style={styles.containerBotao}
+                >
+                
+                
+                <Text style={styles.botaoTexto}>VOLTAR</Text>
+                </TouchableOpacity>
+                </View>
             </View>
             <View style={styles.containerBox}>
+
             </View>
         </View>
     );
 
 }
 const styles = StyleSheet.create({
+    containerBotaoVoltar:{
+        justifyContent:"space-around",
+        paddingHorizontal:15,
+        position:'absolute',
+        width:140,
+        height:50,
+        top: 180,
+        left:215,
+        borderRadius:5,
+        alignSelf:"center",
+        borderWidth: 1,
+        backgroundColor: "#ffffff",
+        borderColor: "#000000",
+        shadowColor: "#000000",
+    },
+    containerBotaoSalvar:{
+        justifyContent:"space-around",
+        paddingHorizontal:15,
+        position:'absolute',
+        width:140,
+        height:50,
+        top: 180,
+        left:55,
+        borderRadius:5,
+        alignSelf:"center",
+        borderWidth: 1,
+        backgroundColor: "#ffffff",
+        borderColor: "#000000",
+        shadowColor: "#000000",
+    },
+    botaoTexto:{
+        fontWeight: "bold",
+        fontSize: 18,
+        textAlign: "center"
+    },
+    containerImagemBotaoEntrar:{
+        width: 20,
+        height: 20,
+        marginRight: 8,
+    },
+    containerBotao:{
+        alignSelf: "center",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#ffffff",
+        borderRadius: 15,
+        width: 100,
+        height:  40,
+        flexDirection: "row",
+    },
+    containerTextoVencimento:{
+        top:2,
+        textDecorationStyle: "dashed",
+        color: "#000000",
+        fontSize: 15,
+        fontWeight: "600",
+    },
+    containerVencimento:{
+        justifyContent:"space-around",
+        paddingHorizontal:15,
+        position:'absolute',
+        width:158,
+        left:250,
+        top: 100,
+        gap: 5,
+        alignSelf:"center",
+    },
+    containerTextoMedico:{
+        top:2,
+        textDecorationStyle: "dashed",
+        color: "#000000",
+        fontSize: 15,
+        fontWeight: "600",
+    },
+    containerMedico:{
+        justifyContent:"space-around",
+        paddingHorizontal:15,
+        position:'absolute',
+        width:250,
+        left:12,
+        top: 100,
+        gap: 5,
+        alignSelf:"center",
+
+    },
+    containerTextoCompriPeriodo:{
+        top:2,
+        textDecorationStyle: "dashed",
+        color: "#000000",
+        fontSize: 15,
+        fontWeight: "600",
+        left:-2
+    },
+    containerInputCompriPeriodo:{
+        width:250,
+        borderWidth: 1,
+        backgroundColor: "#ffffff",
+        borderColor: "#000000",
+        shadowColor: "#000000",
+        elevation: 15,
+        shadowOffset:{width: -1,height: 1},
+        shadowOpacity: 0.9,
+        shadowRadius: 1,
+        fontSize: 15,
+        fontWeight: "600",
+        borderRadius: 3,
+        paddingTop: 5,
+        paddingBottom: 5,
+    },
+    containerCompriPeriodo:{
+        justifyContent:"space-around",
+        paddingHorizontal:15,
+        position:'absolute',
+        width:400,
+        left:"32%",
+        top: "110%",
+        gap: 5,
+        maxWidth: 400,
+        minWidth: 300,
+        alignSelf:"center",
+    },
     containerPeriodo:{
         justifyContent:"space-around",
         paddingHorizontal:15,
         position:'absolute',
         width:200,
-        left:0,
+        left:12,
         top: "190%",
         gap: 5,
         maxWidth: 400,
         minWidth: 300,
         alignSelf:"center",
+        
     },
     containerTexto3:{
         justifyContent:"space-around",
         paddingRight:135,
         position:'absolute',
         width:"100%",
-        left:"7%",
+        left:15,
         top: "-70%",
         gap: 5,
         maxWidth: 400,
@@ -89,21 +244,32 @@ const styles = StyleSheet.create({
         fontSize: 15,
         fontWeight: "600",
     },
-    containerTexto2:{
+    containerTextoUso:{
+    justifyContent:"space-around",
+    left:150,
+    position:'absolute',
+    width:"100%",
+    top: 122,
+    textDecorationStyle: "dashed",
+    color: "#000000",
+    fontSize: 15,
+    fontWeight: "600",
+},
+    containerTextoDosagem:{
         justifyContent:"space-around",
-        paddingRight:135,
         position:'absolute',
         width:"100%",
-        top: "38.5%",
-        gap: 5,
-        maxWidth: 400,
-        minWidth: 300,
-        alignSelf:"center",
-        flexDirection:"row"
-    
+        top: 122,
+        textDecorationStyle: "dashed",
+        color: "#000000",
+        fontSize: 15,
+        fontWeight: "600",
     },
     containerCheckBox:{
-        margin:8
+        width:30,
+        height:30,
+        borderRadius:100,
+        borderWidth:1
     },
     containerTexto:{
         textDecorationStyle: "dashed",
@@ -119,17 +285,17 @@ const styles = StyleSheet.create({
     containerBox:{
         backgroundColor: "#1088c4",
         minHeight:"5%",
-        top:"92%"
+        top:"88%"
     },
     containerMenor:{
         justifyContent:"space-around",
         paddingHorizontal:15,
-        position:'absolute',
         width:"100%",
-        top: "41%",
+        top: "37.5%",
+        right:10,
         gap: 5,
         maxWidth: 400,
-        minWidth: 300,
+        minWidth: 100,
         alignSelf:"center",
         flexDirection:"row"
     },
@@ -149,7 +315,6 @@ const styles = StyleSheet.create({
         maxWidth: 400,
         minWidth: 300,
     },
-
     containerInput:{
         borderWidth: 1,
         backgroundColor: "#ffffff",
@@ -184,7 +349,7 @@ const styles = StyleSheet.create({
     },
     containerInputDosagem:{
         width:100,
-        left:0,
+        left:"50%",
         borderWidth: 1,
         backgroundColor: "#ffffff",
         borderColor: "#000000",
@@ -200,4 +365,4 @@ const styles = StyleSheet.create({
         paddingBottom: 5,
     },
 
-})  
+})   
