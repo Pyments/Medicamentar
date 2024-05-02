6e858e42962cfcdeb78307e7109510c0a18c9241
LisandroVA2002 <llisandrovieira@gmail.com>
2024-05-02 18:07:46 -0300

    feat: Aba emergência criada

--- src/app/(Main)/(TabMenu)/Home.tsx
+++ src/app/(Main)/(TabMenu)/Home.tsx
@@ -69,6 +69,7 @@ export default function Home() {
               source={require("../../../assets/emergencia.png")}
               style={styles.containerBotoes}
             ></Image>
+            
           </TouchableOpacity>
           <TouchableOpacity
             onPress={() => router.navigate({ pathname: "./Configuration" })}
