81aeb4c4c2972fe734ccdddb49f1e2d68793226b
Eduardo-Pyment <edupymota@protonmail.com>
2024-05-02 14:06:43 -0300

    feat: Adiciona cores de accent Adiciona cor de accent para light mode #ADEDFF Adiciona cor de accent para dark mode #4D6385

--- src/constants/ColorTheming.ts
+++ src/constants/ColorTheming.ts
@@ -8,14 +8,17 @@ const colorTheme = {
     text: "#000000",
     primary: "#FFFFFF",
     secondary: "#20A2EB",
+    accent: "#ADEDFF"
   },
   dark: {
     text: "#FFFFFF",
     primary: "#131313",
     secondary: "#656565",
+    accent: "#4D6385"
   }
 }
 
 export const bgThemeColor = colorScheme() ? colorTheme.light.primary : colorTheme.dark.primary;
 export const fgThemeColor = colorScheme() ? colorTheme.light.secondary : colorTheme.dark.secondary;
-export const textThemeColor = colorScheme() ? colorTheme.dark.text : colorTheme.light.text;
+export const textThemeColor = colorScheme() ? colorTheme.light.text : colorTheme.dark.text;
+export const accentThemeColor = colorScheme() ? colorTheme.light.accent : colorTheme.dark.accent;
