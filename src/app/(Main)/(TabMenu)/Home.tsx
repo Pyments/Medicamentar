368402547bab99c553eb309020e91fdc4006e289
Lushlitz <pedhcavdsan1603@gmail.com>
2024-05-14 15:35:37 -0300

    refactor: refatorando a tela home para dar compatibilidade a temas

--- src/app/(Main)/(TabMenu)/Home.tsx
+++ src/app/(Main)/(TabMenu)/Home.tsx
@@ -1,12 +1,14 @@
-import { View, Text, StyleSheet, Image, TouchableOpacity, Modal, SafeAreaView, BackHandler, Alert, Platform, StatusBar } from "react-native";
+import { View, Text, StyleSheet, Image, TouchableOpacity, Modal, SafeAreaView, BackHandler, Alert, Platform, StatusBar, TouchableWithoutFeedback } from "react-native";
 import { router, useNavigation } from "expo-router";
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import * as animatable from "react-native-animatable"
 import { DrawerActions } from "@react-navigation/native";
+import { useFonts } from "expo-font"
+import * as SplashScreen from 'expo-splash-screen';
 
 import NotifiCard from "@/src/components/NotifiCard";
 import Footer from "@/src/components/Footer";
-import { bgThemeColor, fgThemeColor } from "@/src/constants/ColorTheming"
+import { bgThemeColor, fgThemeColor, textThemeColor, secBgThemeColor, accentThemeColor } from "@/src/constants/ColorTheming"
 
 export default function Home(this: any) {
   const [IsModalVisible, setIsModalVisible] = useState(false);
@@ -17,6 +19,19 @@ export default function Home(this: any) {
       navigation.dispatch(DrawerActions.openDrawer());
   };
 
+  const [fontsLoaded, fontError] = useFonts({
+    "armata-regular-400": require("../../../fonts/armata-regular-400.ttf"),
+  });   
+
+  const onLayoutRootView = useCallback(async () => {
+      if (fontsLoaded || fontError) {
+          await SplashScreen.hideAsync();
+      }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+      return null;
+  }
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.containerTopo}>
@@ -56,18 +71,23 @@ export default function Home(this: any) {
       <animatable.View animation="zoomIn" delay={50} style={styles.containerColumns}>
         <View style={styles.containerColumn}>
           <TouchableOpacity
+            style={styles.containerBotoes}
             onPress={() =>
               router.navigate({
                 pathname: "./Exames",
               })
             }
           >
+            <View style={{}}>
             <Image
-              source={require("../../../assets/consultaseExames.png")}
-              style={styles.containerBotoes}
+              source={require("@/src/assets/hospital.png")}
+              style={styles.containerBotoesImage}
             ></Image>
+            </View>
           </TouchableOpacity>
+          <Text style={styles.texto}>Consultas e Exames</Text>
           <TouchableOpacity
+          style={styles.containerBotoes}
           onPress={() =>
             router.navigate({
               pathname: "./Emergencia",
@@ -75,22 +95,25 @@ export default function Home(this: any) {
           }
           >
             <Image
-              source={require("../../../assets/emergencia.png")}
-              style={styles.containerBotoes}
+              source={require("@/src/assets/emergency.png")}
+              style={styles.containerBotoesImage}
             ></Image>
-            
           </TouchableOpacity>
+          <Text style={styles.texto}>Emergência</Text>
           <TouchableOpacity
+            style={styles.containerBotoes}
             onPress={() => router.navigate({ pathname: "./Configuration" })}
           >
             <Image
-              source={require("../../../assets/config.png")}
-              style={styles.containerBotoes}
+              source={require("@/src/assets/configuracoes.png")}
+              style={styles.containerBotoesImage}
             ></Image>
           </TouchableOpacity>
+          <Text style={styles.texto}>Configurações</Text>
         </View>
-        <View>
+        <View style={styles.containerColumn}>
           <TouchableOpacity
+            style={styles.containerBotoes}
             onPress={() =>
               router.navigate({
                 pathname: "./Medicamentos",
@@ -99,18 +122,23 @@ export default function Home(this: any) {
             }
           >
             <Image
-              source={require("../../../assets/medicamentos.png")}
-              style={styles.containerBotoes}
+              source={require("@/src/assets/pilulas.png")}
+              style={styles.containerBotoesImage}
             ></Image>
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => router.navigate({pathname: "./Perfil"})}>
-            <Image
-              source={require("../../../assets/perfil_nome.png")}
-              style={styles.containerBotoes}
-            ></Image>
-            
-          </TouchableOpacity>
+          <Text style={styles.texto}>Medicamentos</Text>
           <TouchableOpacity 
+          onPress={() => router.navigate({pathname: "./Perfil"})}
+          style={styles.containerBotoes}
+          >
+            <Image
+              source={require("@/src/assets/user.png")}
+              style={styles.containerBotoesImage}
+            ></Image>
+          </TouchableOpacity>
+          <Text style={styles.texto}>Perfil</Text>
+          <TouchableOpacity 
+          style={styles.containerBotoes}
           onPress={() => Alert.alert("Deseja sair?", "Você tem certeza que deseja sair do app?",
             [{
               text: "Não",
@@ -123,10 +151,11 @@ export default function Home(this: any) {
             ]
            )}>
             <Image
-              source={require("../../../assets/sair.png")}
-              style={styles.containerBotoes}
+              source={require("@/src/assets/sair_solo.png")}
+              style={styles.containerBotoesImage}
             ></Image>
           </TouchableOpacity>
+          <Text style={styles.texto}>Sair</Text>
         </View>
       </animatable.View>
       <Footer />
@@ -164,16 +193,28 @@ const styles = StyleSheet.create({
     alignSelf: "center",
     alignItems: "center",
     flexDirection: "row",
-    gap: 65,
+    gap: 50,
   },
   containerColumn: {
     flexDirection: "column",
   },
   containerBotoes: {
-    resizeMode: "contain",
-    width: 110,
-    height: 110,
+    alignSelf:"center",
+    alignItems: "center",
+    justifyContent:"center",
+    backgroundColor: `${secBgThemeColor}`,
+    width: 80,
+    height: 80,
     marginBottom: 20,
+    borderWidth: 2.5,
+    borderRadius: 10,
+    borderColor: `${fgThemeColor}`,
+    marginVertical: 10,
+  },
+  containerBotoesImage:{
+    resizeMode: "contain",
+    width:65,
+    height:65
   },
   containerModal:{
     width: "100%",
@@ -200,5 +241,13 @@ const styles = StyleSheet.create({
     fontSize: 15,
     fontWeight: "400",
     color: "#ffffff"
+  },
+  texto:{
+    color: `${textThemeColor}`,
+    alignSelf: "center",
+    fontFamily: "armata-regular-400",
+    fontSize: 14,
+    marginBottom: 10,
+    marginTop: -15
   }
 });
